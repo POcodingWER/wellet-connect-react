110e5f9e860cb5bd23e30c37383a3d4cb7760235
lim.cs <cheolsoon.lim@emotion.co.kr>
2022-08-29 17:11:03 +0900

    정리

---
+++ metamask/src/Test.js
@@ -0,0 +1,103 @@
+import React,{useState,useEffect,useCallback} from 'react'
+
+function Test() {
+  const connectklaytnWellet = async() =>{
+    const klaytnConnectSuccess = await window.klaytn.enable(); //지갑연결
+    if(klaytnConnectSuccess){   //연결되면
+      console.log('현제 지갑주소 :', klaytnConnectSuccess);
+      console.log('네트워크 넘버 :',window.klaytn.networkVersion, window.klaytn.networkVersion===1001?'Testnet':'MainNet' );
+      console.log('선택한 지갑주소:', window.klaytn.selectedAddress);
+    }
+  }
+  const cotractAddress ='0x69f70cc86CD080bB5038332D9c95cA9676Cd7e7A'
+
+  const mint = async ()=>{    //가스비계산 통신 2번
+    console.log('지갑연결유무:', window.klaytn.selectedAddress);
+    const contract = new caverExtKAS.klay.Contract(KIP17.abi, cotractAddress);
+    const balance = await contract.methods.totalSupply().call()
+    
+    console.log('보유토큰',balance); 
+
+    const gas = await contract.methods
+      .mintWithTokenURI(window.klaytn.selectedAddress,balance,'naver.com')
+      .estimateGas({
+        from: window.klaytn.selectedAddress,
+        });
+    console.log(gas);
+    
+    const aprove = await contract.methods
+      .mintWithTokenURI(window.klaytn.selectedAddress,balance,'naver.com')
+      .send({
+        from: window.klaytn.selectedAddress,
+        gas
+        });
+      console.log(aprove);
+  }
+
+  const mint2 = async ()=>{   //계산없이 통신 1번
+    const tokenValue =  caverExtKAS.utils.toPeb(110, 'KLAY')        //faovr 얼마로 할지
+
+    const contract = new caverExtKAS.klay.Contract(KIP17.abi, cotractAddress);
+    const balance = await contract.methods.totalSupply().call()
+    
+    console.log(tokenValue);
+    console.log('보유토큰',balance); 
+
+    const data = await contract.methods
+      .mintWithTokenURI(window.klaytn.selectedAddress,balance,'naver.com')
+      .encodeABI();
+    console.log(data);
+
+    const params = {
+      type: "SMART_CONTRACT_EXECUTION",
+      from: caverExtKAS.utils.toChecksumAddress(window.klaytn.selectedAddress),   //보내는사람주소
+      to: caverExtKAS.utils.toChecksumAddress(cotractAddress),  //컨트랙트주소
+      data,
+    }
+    
+    const res = await caver.klay.sendTransaction({
+      ...params,
+      gas:1829110,
+     });
+    console.log(res);
+  }
+
+  const noSingSendTx = async () => {   //사인없이 트잭보내기
+    /** 키링생성 */
+    const senderKeyring = await caver.wallet.keyring.create(
+      "퍼블키",
+      "프라이빗키"
+    );
+    console.log(senderKeyring);
+    caver.wallet.add(senderKeyring);
+    /** mint1, mint2 랑다름 caver.contract.create을해야 안에 _wallet이 추가되어 있음*/
+    const contract = new caver.contract.create(KIP17.abi, cotractAddress);
+    console.log(contract);
+    const balance = await contract.methods.totalSupply().call();
+    const receipt = await contract.send(
+      {
+        from: senderKeyring.address,
+        gas: 1000000,
+      },
+      "mintWithTokenURI", //메소드
+      senderKeyring.address,
+      balance,
+      "naver.com"
+    );
+    console.log('생성txHash :',receipt.transactionHash);    //딜레이가 조금생김
+  };
+
+  return (
+    <div className="App">
+      <header>
+       <div>
+       <button style={{width:'100px',height:'100px'}} onClick={connectklaytnWellet}>1. 지갑연결</button> <br /><br />
+        <button style={{width:'100px',height:'100px'}} onClick={noSingSendTx}>2. Favor approve</button>
+       </div>
+      
+      </header>
+    </div>
+  );
+}
+
+export default Test;
---
+++ metamask/src/component/GetSaleInfo.jsx
@@ -0,0 +1,58 @@
+import React,{useState,useEffect,useRef} from 'react'
+import MyToken from'../abi/MyToken.json'
+
+export default function GetSaleInfo({caverExtKAS,KIP7Adr,minterAdr}) {
+
+const [setAdr, setSetAdr] = useState('')
+const [setAdr1, setSetAdr1] = useState('')
+
+const setToken =async ()=>{
+  const tokenValue =  caverExtKAS.utils.toPeb(100, 'KLAY')
+  const contract = new caverExtKAS.klay.Contract(MyToken.abi,KIP7Adr); 
+
+  const gas =await contract.methods
+      .mint(setAdr,tokenValue)     //가스비 계산해서
+      .estimateGas({
+        from: window.klaytn.selectedAddress,
+        });
+
+  const mint = await contract.methods
+  .mint(setAdr,tokenValue)
+  .send({
+    from: window.klaytn.selectedAddress,
+    gas
+    });
+    console.log(mint);
+}
+
+const getBalanceOf =async ()=>{
+  const contract = new caverExtKAS.klay.Contract(MyToken.abi,KIP7Adr); 
+  const balanceOf = await contract.methods
+  .balanceOf(setAdr1)
+  .call();
+
+  console.log( await caverExtKAS.utils.fromPeb(balanceOf, 'KLAY'));
+}
+
+const getAllowance =async ()=>{
+  const contract = new caverExtKAS.klay.Contract(MyToken.abi,KIP7Adr); 
+  const balanceOf = await contract.methods
+  .allowance(setAdr1,minterAdr)
+  .call();
+
+  console.log( await caverExtKAS.utils.fromPeb(balanceOf, 'KLAY'));
+}
+
+
+  return (
+    <div>
+        <div style={{fontSize:'20px'}}>
+          <input class="form-control" id="kip7" onChange={(e)=>setSetAdr(e.target.value)} placeholder="0x1234" />
+          <button style={{width:'100px',height:'100px'}} onClick={setToken}> 토큰 넣어줌</button> <br/>
+          <input class="form-control" id="kip7" onChange={(e)=>setSetAdr1(e.target.value)} placeholder="0x1234" />
+          <button style={{width:'100px',height:'100px'}} onClick={getBalanceOf}> 몇개가지고있는지 확인</button>
+          <button style={{width:'100px',height:'100px'}} onClick={getAllowance}> 승인받았는지</button>
+        </div>
+    </div>
+  )
+}
