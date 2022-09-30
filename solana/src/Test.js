3201699069101c5aa5c05ab1ff5b26f991989bee
lim.cs <cheolsoon.lim@emotion.co.kr>
2022-09-30 09:38:30 +0900

    solana 팬텀지갑 연결

---
+++ solana/src/Test.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+if (typeof window.solana !== "undefined") {
+  console.log("phantom is installed!");
+};
+
+function Test() {
+  const [walletAddress, setWalletAddress] = useState(null);
+ 
+  const connectWalletPhantom = async () => {
+    const { solana } = window;
+    console.log(window.solana);
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <div>
+          <button
+            style={{ width: "220px", height: "50px" }}
+            onClick={connectWalletPhantom}
+          >
+            1. 지갑연결
+          </button>
+          <br />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Test;
