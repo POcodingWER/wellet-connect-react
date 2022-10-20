47f8582b4d611db3a676e2c2929f99a71a6c20e2
lim.cs <cheolsoon.lim@emotion.co.kr>
2022-10-20 17:29:53 +0900

    캔디머신 세팅 완료

--- phantom/src/App.js
+++ phantom/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import { keypairIdentity, Metaplex, toBigNumber } from "@metaplex-foundation/js";
+import { nftStorage } from "@metaplex-foundation/js-plugin-nft-storage";
+import { mockStorage, keypairIdentity, Metaplex, toBigNumber, bundlrStorage, toMetaplexFileFromBrowser } from "@metaplex-foundation/js";
 import {
   clusterApiUrl,
   Connection,
@@ -18,7 +19,7 @@ function App() {
     "32WsxRK1vYia8Bk1WfDaoKdtdmVJbYFCZuh6Q58yhHPr"
   );
   const [nft, setNft] = useState(null);
-  const [walletAddress, setWalletAddress] = useState("");
+  const [payer, setpayer] = useState("");
   const [candyMachineAdr, setCandyMachineAdr] = useState('');
 
   const fetchNft = async () => {
@@ -50,28 +51,26 @@ function App() {
     //     121, 35, 172, 247, 68, 251, 226, 218, 48, 63, 176, 109, 168, 89, 238, 135,
     //   ])
     // );
-    setWalletAddress(publickey);
+    setpayer(publickey);
     console.log(publickey.publicKey.toBase58());
-    const airdropSignature = await connection.requestAirdrop(
-      publickey.publicKey,
-      LAMPORTS_PER_SOL * 2
-    );
-    console.log(
-      "드뢉완료,",
-      await connection.confirmTransaction(airdropSignature)
-    );
+    // const airdropSignature = await connection.requestAirdrop(
+    //   publickey.publicKey,
+    //   LAMPORTS_PER_SOL * 2
+    // );
+    // console.log(
+    //   "드뢉완료,",
+    //   await connection.confirmTransaction(airdropSignature)
+    // );
   };
   /** 컬랙션 만들고 캔디머신 만들기 */
   const makeCandyMachine = async () => {
     console.log("loading");
     //돈낼사람 넣어주고
-    metaplex = metaplex
-    .use(keypairIdentity(walletAddress))
-    // .use(mockStorage());
+    metaplex.use(keypairIdentity(payer));
 
     const { nft: collectionNft } = await metaplex.nfts().create({
       isCollection: true,
-      name: 'My Collection11',
+      name: 'uuuuuuuuuuuu',
       uri: "https://arweave.net/E7ZchKO6QgNKMBtapymZGQDXdm7b1_swQ0AyjmBB_DA", //json 형식 메타데이터
       sellerFeeBasisPoints: 0,
     });
@@ -81,26 +80,316 @@ function App() {
       collectionNft
     );
   
-
+    
     const { candyMachine } = await metaplex.candyMachines().create({
+      sellerFeeBasisPoints: 333, // 3.33%
+      itemsAvailable: toBigNumber(6),  //몇개 발행
       collection: {
         address: collectionNft.address,
         updateAuthority: metaplex.identity(),
       },
-      sellerFeeBasisPoints: 333, // 3.33%
-      itemsAvailable: toBigNumber(50),
     });
 
-    console.log(candyMachine);
     console.log(
-      `캔디머신주소: ${candyMachine.address.toBase58()}\ncollectionMintAddress: ${candyMachine.collectionMintAddress.toBase58()}\nAuthorityAddress: ${collectionNft.authorityAddress?.toBase58()}`,
+      `캔디머신주소: ${candyMachine.address.toBase58()}
+      캔디머신권한: ${candyMachine.authorityAddress.toString()}
+      collectionMintAddress: ${candyMachine.collectionMintAddress.toBase58()}
+      NFT총 가능양: ${candyMachine.itemsAvailable.toString()}
+      지금까지 발행양: ${candyMachine.itemsMinted.toString()}
+      남은 발행양: ${candyMachine.itemsRemaining.toString()}
+      `,
       candyMachine
     );
     setCandyMachineAdr(candyMachine);
   };
+  /** 캔디머신 가져오고 권한업데이트 */
+  const getCandyMAandUpdateAuthority = async () => {
+    metaplex.use(keypairIdentity(payer))
 
+    const candyMachine = await metaplex.candyMachines().findByAddress({
+      address: new PublicKey(candyMachineAdr.address.toBase58()),
+    });
+    console.log(
+      "찾은 캔디머신 주소",
+      candyMachine.address.toBase58(),
+      "캔디머신 권한자",
+      candyMachine.authorityAddress.toString()
+    );
+
+    const newAuthority = Keypair.generate();
+    console.log("권한 변경할 Account", newAuthority.publicKey.toBase58());
+
+    const tx = await metaplex.candyMachines().update({
+      candyMachine,
+      authority: payer,
+      newAuthority: newAuthority.publicKey,
+    });
+    console.log("변경 tx 주소",tx);
+
+    const updatedCandyMachine = await metaplex
+      .candyMachines()
+      .refresh(candyMachine);
+    console.log("변겨완료", updatedCandyMachine.authorityAddress.toString());
+  };
+  /** NFT데이터 업데이트 */
+  const NFTdataUpdate = async () => {
+    metaplex.use(keypairIdentity(payer))
+
+    const candyMachine = await metaplex.candyMachines().findByAddress({
+      address: new PublicKey(candyMachineAdr.address.toBase58()),
+    });
+    console.log(
+      "찾은 캔디머신 주소",
+      candyMachine.address.toBase58(),
+      "캔디머신 권한자",
+      candyMachine.authorityAddress.toString()
+    );
+    
+    const newCreator = Keypair.generate();
+    console.log("2차수수료 변경할 Account", newCreator.publicKey.toBase58());
+
+    const tx = await metaplex.candyMachines().update({
+      candyMachine,
+      symbol: 'NEW',
+      sellerFeeBasisPoints: 100,
+      creators: [{ address: newCreator.publicKey, share: 100 }],
+    });
+    console.log("변경 tx 주소",tx);
+
+    const updatedCandyMachine = await metaplex
+      .candyMachines()
+      .refresh(candyMachine);
+    console.log("변경된 2차수수료 받을 Account", updatedCandyMachine.creators[0].address.toBase58());
+  }
+  /** 민터(컬랙션) 변경*/
+  const candyMachineChangeMinter = async () => {
+    metaplex.use(keypairIdentity(payer))
+
+    const candyMachine = await metaplex.candyMachines().findByAddress({
+      address: new PublicKey(candyMachineAdr.address.toBase58()),
+    });
+    console.log(
+      "찾은 캔디머신 주소",
+      candyMachine.address.toBase58(),
+      "캔디머신 권한자",
+      candyMachine.authorityAddress.toString(),
+      "컬랙션주소",
+      candyMachine.collectionMintAddress.toBase58(),
+    );
+    
+    const tx = await metaplex.candyMachines().update({
+      candyMachine,
+      collection: {
+        address:new PublicKey('3i7izYHug4t34mFWqXyV74qmZiM2XkmnXvHNJvE5FmzY'), //바꿀민터
+        updateAuthority: metaplex.identity(), //업데이트 권한 확인인가봄?
+      },
+    });
+    console.log("변경 tx 주소",tx);
+
+    const updatedCandyMachine = await metaplex
+      .candyMachines()
+      .refresh(candyMachine);
+    console.log(
+      "변경된 컬랙션 주소",
+      updatedCandyMachine.collectionMintAddress.toString()
+    );
+
+  }
+  /** 아이템 목록 설정 */
+  const itemUpdate = async () => {
+    metaplex.use(keypairIdentity(payer))
+
+    const candyMachine = await metaplex.candyMachines().findByAddress({
+      address: new PublicKey(candyMachineAdr.address.toBase58()),
+    });
+    console.log(
+      "찾은 캔디머신 주소",
+      candyMachine.address.toBase58(),
+      "캔디머신 권한자",
+      candyMachine.authorityAddress.toString(),
+    );
+
+    const tx = await metaplex.candyMachines().update({
+      candyMachine,
+      itemSettings: {
+        type: "configLines",        //configLines or hidden
+        prefixName: "My New NFT #$ID+1$",  
+        nameLength: 0,
+        prefixUri: "https://arweave.net/",
+        uriLength: 43,
+        isSequential: true,   //nft를 순차적으로 생성할지:true 램덤하게 생성할지:false
+      },
+    });
+
+    console.log("변경 tx 주소",tx);
+    const updatedCandyMachine = await metaplex
+      .candyMachines()
+      .refresh(candyMachine);
+    console.log("변경된 컬랙션 주소", updatedCandyMachine);
+  }
+  /** 캔디머신 삭제하기 */
+  const candyMachineDelete = async () => {
+    metaplex.use(keypairIdentity(payer))
+
+    const candyMachine = await metaplex.candyMachines().findByAddress({
+      address: new PublicKey(candyMachineAdr.address.toBase58()),
+    });
+    console.log(
+      "찾은 캔디머신 주소",
+      candyMachine.address.toBase58(),
+      "캔디머신 가드 주소",
+      candyMachine.candyGuard.address.toString(),
+    );
+    // 캔디머신만 지워짐
+    // await metaplex.candyMachines().delete({
+    //   candyMachine: candyMachine.address,
+    // });
+
+    // 연결된 가드까지 다삭제
+    await metaplex.candyMachines().delete({
+      candyMachine: candyMachine.address,
+      candyGuard: candyMachine.candyGuard.address,
+    });
+    setCandyMachineAdr('')
+    console.log('완료');
+  }
+  /** json meta data upload */
+  const metadataUpload = async () => {
+    console.log('****머가 문제인지 업로드안됨, 근데 우리는 메타데이터 다만들고 올릴꺼니깐 2번부터 진행 나중에 1번 해보자');
+    //metaplex  기초
+    //https://user-images.githubusercontent.com/3642397/167716670-16c6ec5e-76ba-4c15-9dfc-7790d90099dd.png
+    console.log('default',metaplex); 
+    console.log('default',metaplex.identity());
+    metaplex.use(keypairIdentity(payer)).use(nftStorage());
+    console.log('키페어세팅후',metaplex.identity());
+    console.log(metaplex);
+    const { uri } = await metaplex.nfts().uploadMetadata({
+      name: "My NFT #1",
+      description: "제발요",
+      // seller_fee_basis_points:200,
+      // image: "https://co3fzbfdxjbagsrqdnnkokmzdeano5to3pl7wmcdiazi4ycb7qya.arweave.net/E7ZchKO6QgNKMBtapymZGQDXdm7b1_swQ0AyjmBB_DA",
+      // external_url:'https://explorer.solana.com/address/GLeayYXBjWEvb1T6GDrp2SJFkKTVZGxCJ77Hv5qSJxWq?cluster=devnet',
+    });
+    console.log(uri);
+  }
+  /** 캔디머신에 이름과 uri 넣기*/
+  const CMinsertItems = async () => {
+    metaplex.use(keypairIdentity(payer));
+
+    const candyMachine = await metaplex.candyMachines().findByAddress({
+      address: new PublicKey(candyMachineAdr.address.toBase58()),
+    });
+    console.log(
+      "찾은 캔디머신 주소",
+      candyMachine.address.toBase58(),
+      candyMachine.items
+    );
+
+    await metaplex.candyMachines().insertItems({
+      candyMachine,
+      items: [
+        { name: "bellygom #1", uri: "https://belly.bellygom.world/1.json" },
+        { name: "bellygom #2", uri: "https://belly.bellygom.world/2.json" },
+        { name: "bellygom #3", uri: "https://belly.bellygom.world/3.json" },
+      ],
+    });
+    let updatedCandyMachine = await metaplex
+      .candyMachines()
+      .refresh(candyMachine);
+    console.log('insertItems 진행중',updatedCandyMachine.items);
+
+    //데이터 크기가 커서 다안들어가면 인덱스 사용해서 넣어주면됨 설정 수량을 초과할수는없다.
+    await metaplex.candyMachines().insertItems({
+      candyMachine,
+      index:3,
+      items: [
+        { name: "bellygom #4", uri: "https://belly.bellygom.world/4.json" },
+        { name: "bellygom #5", uri: "https://belly.bellygom.world/5.json" },
+        { name: "bellygom #6", uri: "https://belly.bellygom.world/6.json" },
+      ],
+    });
+     updatedCandyMachine = await metaplex
+    .candyMachines()
+    .refresh(candyMachine);
+    console.log('insertItems 완료',updatedCandyMachine.items);
+  };
+  /** 아이템 이름 uri 설정되어있을때 항목삽입법 */
+  const itemSettingsCMinsertItems = async () => {
+    metaplex.use(keypairIdentity(payer));
+
+    const candyMachine = await metaplex.candyMachines().findByAddress({
+      address: new PublicKey(candyMachineAdr.address.toBase58()),
+    });
+    console.log(
+      "찾은 캔디머신 주소",
+      candyMachine.address.toBase58(),
+      candyMachine.itemSettings
+    );
+
+    await metaplex.candyMachines().update({
+      candyMachine,
+      itemsAvailable: toBigNumber(6),
+      itemSettings: {
+        type: "configLines",
+        prefixName: "belly gom #",
+        nameLength: 1,
+        prefixUri: "https://belly.bellygom.world/",
+        uriLength: 6,
+        isSequential: true,
+      },
+    });
+
+    let updatedCandyMachine = await metaplex
+      .candyMachines()
+      .refresh(candyMachine);
+    console.log("캔디머시 아이템세팅", updatedCandyMachine.itemSettings);
+
+    await metaplex.candyMachines().insertItems({
+      candyMachine,
+      items: [
+        { name: "1", uri: "1.json" },
+        { name: "2", uri: "2.json" },
+        { name: "3", uri: "3.json" },
+      ],
+    });
+
+    updatedCandyMachine = await metaplex.candyMachines().refresh(candyMachine);
+    console.log("캔디머시 아이템인설트", updatedCandyMachine.items);
+  };
+  /** 아이템 인설트후 수정 방법*/
+  const insertItemsModify = async () => {
+    metaplex.use(keypairIdentity(payer))
+
+    const candyMachine = await metaplex.candyMachines().findByAddress({
+      address: new PublicKey(candyMachineAdr.address.toBase58()),
+    });
+    if (candyMachine.itemsAvailable.toString()==candyMachine.items.length) {
+      return alert('최대발행수까지 세팅되면 수정불가능');
+    }
+    console.log(
+      "찾은 캔디머신 주소",
+      candyMachine.address.toBase58(),
+      candyMachine.itemSettings,
+      candyMachine.items
+    );
+    
+    await metaplex.candyMachines().insertItems({
+      candyMachine,
+      index: 1,
+      items: [
+        { name: "d", uri: "d.json" }
+      ],
+    });
+
+    let updatedCandyMachine = await metaplex
+      .candyMachines()
+      .refresh(candyMachine);
+      console.log("캔디머시 아이템수정", updatedCandyMachine.items);
+  }
+  /** */
   const test = ()=>{
-    console.log(toBigNumber(50));
+    console.log(new PublicKey('CvPTw4BVuiNMa7Yc1JJTAt6QLdeGoHyLtpY8Vojajteh'));
+    
   }
   return (
     <div className="App">
@@ -137,7 +426,7 @@ function App() {
         >
           1. 비밀키로 키페어 만들기
         </button>
-        {walletAddress.publicKey?.toBase58()}
+        {payer.publicKey?.toBase58()}
         <br />
         <button
           style={{ width: "220px", height: "50px" }}
@@ -146,6 +435,70 @@ function App() {
           2. 민터생성 후 캔디머신v3생성
         </button>
         {candyMachineAdr?.address?.toBase58()}
+        <br />
+        <button
+          style={{ width: "220px", height: "50px" }}
+          onClick={getCandyMAandUpdateAuthority}
+        >
+          3.캔디머신 찾은후 권한 업데이트
+        </button>
+        <br />
+        <button
+          style={{ width: "220px", height: "50px" }}
+          onClick={NFTdataUpdate}
+        >
+          4.캔디머신 찾은후 NFTdata 엡데이트
+        </button>
+        <br />
+        <button
+          style={{ width: "220px", height: "50px" }}
+          onClick={candyMachineChangeMinter}
+        >
+          5.캔디머신 찾은후 민터(컬랙션) 업데이트
+        </button>
+        <br />
+        <button
+          style={{ width: "220px", height: "50px" }}
+          onClick={itemUpdate}
+        >
+          6.캔디머신 찾은후 아이템 업데이트
+        </button>
+        <br />
+        <button
+          style={{ width: "220px", height: "50px" }}
+          onClick={candyMachineDelete}
+        >
+          7.캔디머신 찾은후 삭제하기
+        </button>
+        <br />
+        <br />
+        <button
+          style={{ width: "220px", height: "50px" }}
+          onClick={metadataUpload}
+        >
+          1.메타데이터 업로드 지금안됨 나중에해보자 
+        </button>
+        <br />
+        <button
+          style={{ width: "220px", height: "50px" }}
+          onClick={CMinsertItems}
+        >
+          2.캔디머신에 아이템세팅x 이름과 메타데이터 넣기
+        </button>
+        <br />
+        <button
+          style={{ width: "220px", height: "50px" }}
+          onClick={itemSettingsCMinsertItems}
+        >
+          3.캔디머신에 아이템세팅한후 이름,메타데이터 넣기
+        </button>
+        <br />
+        <button
+          style={{ width: "220px", height: "50px" }}
+          onClick={insertItemsModify}
+        >
+          4.캔디머신에 아이템 수정
+        </button>
         <br />
         <button
           style={{ width: "220px", height: "50px" }}
